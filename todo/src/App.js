e871107d6a8057efc66e3f2ef446691c797e4f10
IvanSlivenko <ivan8822@ukr.net>
2025-01-03 18:02:47 +0300

    03-01-2024-18-02

---
+++ todo/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div className="wrapper">
+      <input
+      placeholder='Enter something...' 
+        type="text" 
+      />
+      <button className='enter'>ENTER</button>
+    </div>
+
+    </div>
+  );
+}
+
+export default App;
